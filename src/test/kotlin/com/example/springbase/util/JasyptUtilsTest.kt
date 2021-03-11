eb9d9b2cce9e7d27fa1b7e48c0b6a02c8f53c304
김정훈 <junghoon15@rsquare.co.kr>
2021-03-11 14:29:02 +0900

    database 연동

--- src/test/kotlin/com/example/springbase/util/JasyptUtilsTest.kt
+++ src/test/kotlin/com/example/springbase/util/JasyptUtilsTest.kt
@@ -15,15 +15,9 @@ import org.springframework.test.context.junit4.SpringRunner
 class JasyptUtilsTest {
     @Test
     fun encryptTest(){
-        val res = JasyptUtils.encrypt("gomip")
-        val res2 = JasyptUtils.encrypt("rkskek12")
-        println("encrypt : $res")
-        println("encrypt : $res2")
     }
 
     @Test
     fun decryptTest(){
-        val res = JasyptUtils.decrypt("Bg9Sj7R3o/3RG/zR1fdqe3y/bc7ElPHAgRVYMBFkyL3D9C8HIsF7AVMw0oxrSfVyf8PhgQYGH4Xvs5MySpN40zfXPTmB8lRUSsUWP38mi8qlx1ueb/P334Pm84lwUrv1")
-        println("decrypt : $res")
     }
 }
