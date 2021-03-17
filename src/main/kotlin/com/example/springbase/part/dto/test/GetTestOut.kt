316a51ccc01e60ee8314db44d4a36c7e7bbef676
김정훈 <junghoon15@rsquare.co.kr>
2021-03-17 14:21:08 +0900

    페이징 처리 및 / gitignore 로그폴더 제거

--- src/main/kotlin/com/example/springbase/part/dto/test/GetTestOut.kt
+++
@@ -1,9 +0,0 @@
-package com.example.springbase.part.dto.test
-
-/**
- * 2021.01.05 | gomip | created
- */
-data class GetTestOut (
-    val id      : Int       = 0,
-    val value   : String    = ""
-)
--- src/main/kotlin/com/example/springbase/part/dto/test/PostTestIn.kt
+++
@@ -1,6 +0,0 @@
-package com.example.springbase.part.dto.test
-
-data class PostTestIn (
-        var id: Int = 0,
-        var value: String = ""
-)
