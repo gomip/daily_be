1b7f749fca3f36195dd5ada3bf3b1606cc046e9f
김정훈 <junghoon15@rsquare.co.kr>
2021-03-17 18:30:03 +0900

    주석 추가

--- src/main/kotlin/com/example/springbase/part/service/qus/QusService.kt
+++ src/main/kotlin/com/example/springbase/part/service/qus/QusService.kt
@@ -9,11 +9,17 @@ import com.example.springbase.repo.mybatis.qus.QusMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
+/**
+ * 2021.03.17 | gomip | created
+ * => paging 서비스 제작
+ */
 @Service
 class QusService : BaseService(){
     @Autowired lateinit var mapper: QusMapper
 
     fun selectQusPaging(input: GetQusIn): Paging<GetQusOut> {
+        // tagCd , difCd가 각각 string으로 넘어오기 때문에 리스트 변수들을 각각 만들어준다.
+        // => 왜인지 모르겠지만 query string으로 리스트를 받으면 a|b 형태를 가지게 되고, | 이녀석을 regex으로 잡을수가 없음
         var tagCdList = listOf<String>()
         if (input.tagCd != null) {
             tagCdList = input.tagCd!!.split(",")
